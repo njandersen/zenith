b39c4f0b1433bc87efd81d4daeb25500df1a6c72
njandersen <njandersen22@gmail.com>
2024-01-13 09:57:31 -0500

    add window manager class

--- managers.py
+++ managers.py
@@ -50,4 +50,124 @@ class CaptureManager(object):
     def isWritingVideo(self):
         return self._videoFilename is not None
     
-    def enterFrame
+    def enterFrame(self):
+        """Capture the next frame, if any."""
+
+        # But first, check that any previous frame was exited.
+        assert not self._enteredFrame, \
+            'previous enterFrame() had no matching exitFrame()'
+        
+        if self._capture is not None:
+            self._enteredFrame = self._capture.grab()
+
+    def exitFrame(self):
+        """Draw to the window. Write to files. Release the frame."""
+
+        # Check whether any grabbed frame is retrievable.
+        # The getter may retrieve and cache the frame.
+        if self.frame is None:
+            self._enteredFrame = False
+            return
+        
+        # Update the FPS estimate and related variables.
+        if self._framesElapsed == 0:
+            self._startTime = time.time()
+        
+        else:
+            timeElapsed = time.time() - self._startTime
+            self._fpsEstimate = self._framesElapsed / timeElapsed
+        
+        self._framesElapsed += 1
+
+        # Draw to the window, if any.
+        if self.previewWindowManager is not None:
+            if self.shouldMirrorPreview:
+                mirroredFrame = cv2.flip(self._frame, 1)
+                self.previewWindowManager.show(mirroredFrame)
+            
+            else:
+                self.previewWindowManager.show(self._frame)
+        
+        # Write to the image file, if any.
+        if self.isWritingImage:
+            cv2.imwrite(self._imageFilename, self._frame)
+            self._imageFilename = None
+        
+        # Write to the video file, if any.
+        self._writeVideoFrame()
+
+        # Release the frame.
+        self._frame = None
+        self._enteredFrame = False
+
+    def writeImage(self, filename):
+        """Write the next exited frame to an image file."""
+        self._imageFilename = filename
+
+    def startWritingVideo(
+            self, filename, 
+            encoding = cv2._videoWriter_fourcc('M', 'J', 'P', 'G')):
+        """Start writing exited frames to a video file."""
+        self._videoFilename = filename
+        self._videoEncoding = encoding
+
+    def stopWritingVideo(self):
+        """Stop writing exited frames to a video file."""
+        self._videoFilename = None
+        self._videoEncoding = None
+        self._videoWriter = None
+
+    def _writeVideoFrame(self):
+
+        if not self.isWritingVideo:
+            return
+        
+        if self._videoWriter is None:
+            fps = self._capture.get(cv2.CAP_PROP_FPS)
+            if fps == 0.0:
+                # The capture's FPS is unknown so use an estimate.
+                if self._framesElapsed < 20:
+                    # Wait until more frames elapse so that the 
+                    # estimate is more stable.
+                    return
+                
+                else:
+                    fps = self._fpsEstimate
+            size = (int(self._capture.get(
+                        cv2.CAP_PROP_FRAME_WIDTH)), 
+                    int(self._capture.get(
+                        cv2.CAP_PROP_FRAME_HEIGHT)))
+            self._videoWriter = cv2.VideoWriter(
+                self._videoFilename, self._videoEncoding, fps, size)
+            
+        self._videoWriter.write(self._frame)
+
+class WindowManager(object):
+
+    def __init__(self, windowName, keypressCallback = None):
+        self.keypressCallback = keypressCallback
+
+        self._windowName = windowName
+        self._isWindowCreated = False
+
+    @property
+    def isWindowCreated(self):
+        return self._isWindowCreated
+    
+    def createWindow(self):
+        cv2.namedWindow(self._windowName)
+        self._isWindowCreated = True
+
+    def show(self, frame):
+        cv2.imshow(self._windowName, frame)
+
+    def destroyWindow(self):
+        cv2.destroyWindow(self._windowName)
+        self._isWindowCreated = False
+
+    def processEvents(self):
+        keycode = cv2.waitKey(1)
+        if self.keypressCallback is not None and keycode != -1:
+            self.keypressCallback(keycode)
+
+    
